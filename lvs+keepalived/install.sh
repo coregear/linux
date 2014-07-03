a44f729020a577144f5a3aae9b8704954c67bfb5
shidg <shjiayeah@gmail.com>
2014-07-03 09:35:17 +0800

    lvs

--- lvs+keepalived/install.sh
+++ lvs+keepalived/install.sh
@@ -9,6 +9,17 @@ tar zxvf ipvsadm-1.26.tar.gz && cd ipvsadm-1.26
 make && make install
 
 
+####real_server的sysctl.conf######
+
+##必须关闭arp解析功能##
+net.ipv4.conf.lo.arp_ignore = 1
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_ignore = 1
+net.ipv4.conf.all.arp_announce = 2
+
+
+
+
 #keepalived
 
 tar zxvf keepalived-1.2.8.tar.gz && cd keepalived-1.2.8
